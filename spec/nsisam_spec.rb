02f057b130e0a32cb6bf5b45ddbddae2c6d2e4a1
Rodrigo Manhães <rmanhaes@gmail.com>
2012-07-27 00:53:23 -0300

    encoda arquivos automaticamente -- falta teste de integracao

--- spec/nsisam_spec.rb
+++ spec/nsisam_spec.rb
@@ -1,4 +1,5 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require 'base64'
 
 describe NSISam do
   before :all do
@@ -27,6 +28,16 @@ describe NSISam do
       response.should_not be_nil
       response.should have_key("key")
       response.should have_key("checksum")
+    end
+
+    context "file" do
+      it "encodes content before storing" do
+        Base64.should_receive(:encode64).with(example_file_content).
+          and_return(:dummy_value)
+        @nsisam.should_receive(:store).with(doc: :dummy_value).
+          and_return(:dummy_result)
+        @nsisam.store_file(example_file_content).should == :dummy_result
+      end
     end
   end
 
@@ -65,6 +76,17 @@ describe NSISam do
     it "raises error when key not found" do
       expect { @nsisam.get("i dont exist") }.to raise_error(NSISam::Errors::Client::KeyNotFoundError)
     end
+
+    context 'file' do
+      it 'decodes content after retrieving' do
+        @nsisam.should_receive(:get).with(:key, nil).
+          and_return('data' => :dummy_value)
+        Base64.should_receive(:decode64).with(:dummy_value).
+          and_return(:decoded_dummy)
+        response = @nsisam.get_file(:key)
+        response['data'].should == :decoded_dummy
+      end
+    end
   end
 
   context "updating" do
@@ -78,6 +100,17 @@ describe NSISam do
 
     it "raises error when key not found" do
       expect { @nsisam.update("dont exist ruby is fast", "foo") }.to raise_error(NSISam::Errors::Client::KeyNotFoundError)
+    end
+
+    context 'file' do
+      it 'encodes content before updating' do
+        key = @nsisam.store_file(example_file_content)['key']
+        Base64.should_receive(:encode64).with(:dummy_content).
+          and_return(:dummy_content)
+        @nsisam.should_receive(:update).with(key, :dummy_content).
+          and_return(:dummy_result)
+        @nsisam.update_file(key, :dummy_content).should == :dummy_result
+      end
     end
   end
 
